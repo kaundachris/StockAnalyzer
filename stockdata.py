fb8f563f29c7803fc4d3b5270e5d0e9a63bfbfcc
Chris Kaunda <120592247+kaundachris@users.noreply.github.com>
2025-09-18 11:28:35 +0300

    Add files via upload

--- stockdata.py
+++ stockdata.py
@@ -16,44 +16,66 @@ class Stock():
     
     # Get the company's name
     def company_name(self):
-        return self.stock_data.info["longName"]
+        if self.stock_data.info["longName"]:
+            return self.stock_data.info["longName"]
+        return self.ticker
     
     # get the company's industry
     def company_industry(self):
-        return self.stock_data.info["industry"]
+        if self.stock_data.info["industry"]:
+            return self.stock_data.info["industry"]
+        return "N/A"
     
     # get the company's foward PE
     def company_forward_pe(self):
-        return round(self.stock_data.info["forwardPE"], 2)
+        if self.stock_data.info["forwardPE"]:
+            return round(self.stock_data.info["forwardPE"], 2)
+        return 0
     
     # get the company's earnings growth
     def company_earnings_growth(self):
-        return round(self.stock_data.info["earningsGrowth"] * 100, 2)
+        if self.stock_data.info["earningsGrowth"]:
+            return round(self.stock_data.info["earningsGrowth"] * 100, 2)
+        return 0
     
     # get the company's profit margin
     def company_profit_margin(self):
-        return round(self.stock_data.info["profitMargins"] * 100, 2)
+        if self.stock_data.info["profitMargins"]:
+            return round(self.stock_data.info["profitMargins"] * 100, 2)
+        return 0
 
     # get the company's market capitalization
     def company_market_cap(self):
-        return self.stock_data.info["marketCap"]
+        if self.stock_data.info["marketCap"]:
+            return self.stock_data.info["marketCap"]
+        return 0
     
     # get the company's price to book value
     def company_book_value(self):
-        return round(self.stock_data.info["bookValue"], 2)
+        if self.stock_data.info["bookValue"]:
+            return round(self.stock_data.info["bookValue"], 2)
+        return 0
     
     # get the company's price to book ratio
     def company_pb_ratio(self):
-        return round(self.stock_data.info["priceToBook"], 2)
+        if self.stock_data.info["priceToBook"]:
+            return round(self.stock_data.info["priceToBook"], 2)
+        return 0
     
     # get the company's quick ratio
     def company_quick_ratio(self):
-        return round(self.stock_data.info["quickRatio"], 2)
+        if self.stock_data.info["quickRatio"]:
+            return round(self.stock_data.info["quickRatio"], 2)
+        return 0
     
     # get the company's current ratio
     def company_current_ratio(self):
-        return round(self.stock_data.info["currentRatio"], 2)
+        if self.stock_data.info["currentRatio"]:
+            return round(self.stock_data.info["currentRatio"], 2)
+        return 0
     
     # get the company's free cashflow 
     def company_free_cashflow(self):
-        return round(self.stock_data.info["freeCashflow"], 2)
+        if self.stock_data.info["freeCashflow"]:
+            return round(self.stock_data.info["freeCashflow"], 2)
+        return 0
