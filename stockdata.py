3a307ae97720efe53d39805bb4a7dcf4ca7b4998
Chris Kaunda <120592247+kaundachris@users.noreply.github.com>
2025-09-18 11:51:25 +0300

    Add files via upload

--- stockdata.py
+++ stockdata.py
@@ -16,66 +16,77 @@ class Stock():
     
     # Get the company's name
     def company_name(self):
-        if self.stock_data.info["longName"]:
-            return self.stock_data.info["longName"]
-        return self.ticker
+        name = self.stock_data.info.get("longName")
+        if name is not None:
+            return name
+        return "N/A"
     
     # get the company's industry
     def company_industry(self):
-        if self.stock_data.info["industry"]:
-            return self.stock_data.info["industry"]
+        industry = self.stock_data.info.get("industry")
+        if industry is not None:
+            return industry
         return "N/A"
     
     # get the company's foward PE
     def company_forward_pe(self):
-        if self.stock_data.info["forwardPE"]:
-            return round(self.stock_data.info["forwardPE"], 2)
+        forward_pe = self.stock_data.info.get("forwardPE")
+        if forward_pe is not None:
+            return round(forward_pe, 2)
         return 0
     
     # get the company's earnings growth
     def company_earnings_growth(self):
-        if self.stock_data.info["earningsGrowth"]:
-            return round(self.stock_data.info["earningsGrowth"] * 100, 2)
+        earnings_growth = self.stock_data.info.get("earningsGrowth")
+        if earnings_growth is not None:
+            return round(earnings_growth * 100, 2)
         return 0
     
     # get the company's profit margin
     def company_profit_margin(self):
-        if self.stock_data.info["profitMargins"]:
-            return round(self.stock_data.info["profitMargins"] * 100, 2)
+        profit_margin = self.stock_data.info.get("profitMargins")
+        if profit_margin is not None:
+            return round(profit_margin * 100, 2)
         return 0
 
     # get the company's market capitalization
     def company_market_cap(self):
-        if self.stock_data.info["marketCap"]:
-            return self.stock_data.info["marketCap"]
+        market_cap = self.stock_data.info.get("marketCap")
+        if market_cap is not None:
+            return market_cap
         return 0
     
     # get the company's price to book value
     def company_book_value(self):
-        if self.stock_data.info["bookValue"]:
-            return round(self.stock_data.info["bookValue"], 2)
+        book_value = self.stock_data.info.get("bookValue")
+        if book_value is not None:
+            return round(book_value, 2)
         return 0
     
     # get the company's price to book ratio
     def company_pb_ratio(self):
-        if self.stock_data.info["priceToBook"]:
-            return round(self.stock_data.info["priceToBook"], 2)
+        pb_ratio = self.stock_data.info.get("priceToBook")
+        if pb_ratio is not None:
+            return round(pb_ratio, 2)
         return 0
     
     # get the company's quick ratio
     def company_quick_ratio(self):
-        if self.stock_data.info["quickRatio"]:
-            return round(self.stock_data.info["quickRatio"], 2)
+        quick_ratio = self.stock_data.info.get("quickRatio")
+        if quick_ratio is not None:
+            return round(quick_ratio, 2)
         return 0
     
     # get the company's current ratio
     def company_current_ratio(self):
-        if self.stock_data.info["currentRatio"]:
-            return round(self.stock_data.info["currentRatio"], 2)
+        current_ratio = self.stock_data.info.get("currentRatio")
+        if current_ratio is not None:
+            return round(current_ratio, 2)
         return 0
     
     # get the company's free cashflow 
     def company_free_cashflow(self):
-        if self.stock_data.info["freeCashflow"]:
-            return round(self.stock_data.info["freeCashflow"], 2)
+        free_cashflow = self.stock_data.info.get("freeCashflow")
+        if free_cashflow is not None:
+            return round(free_cashflow, 2)
         return 0
