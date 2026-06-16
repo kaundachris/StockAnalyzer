49ed3039a03fdbda10587cd00ba5cd48d6df9095
Chris Kaunda <120592247+kaundachris@users.noreply.github.com>
2026-06-16 15:01:24 +0300

    Add files via upload

--- company_check.py
+++ company_check.py
@@ -1,4 +1,4 @@
-import requests
+import yfinance as yf
 
 """
     Checks the existence of a company
@@ -15,14 +15,6 @@ class CompanyLookup():
             "nvidia": "NVDA"
         }
 
-        # browser identifier to prevent Yahoo from blocking the request
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-        }
-
-
     # check for existence of company
     def company_exists(self, company_name: str):
         # clean company name
@@ -34,40 +26,10 @@ class CompanyLookup():
 
         # if it is in the list of companies already, return the corresponding ticker
         if company_name in self.COMPANY_TICKERS:
-            return self.COMPANY_TICKERS.get(company_name)
+            return self.COMPANY_TICKERS[company_name]
 
-        # url for the search API for Yahoo Finance
-        # go to the "search" endpoint
-        url = "https://query2.finance.yahoo.com/v1/finance/search"
-
-        # pass the company who's data you want retrieved
-        parameters = {
-            # what company you are searching? 
-            "q": company_name,
-            # this minimizes the quotes you get back
-            "quotesCount": 5,
-            # this minimizes the news results you get back
-            "newsCount": 0,
-        }
-
-        # make the actual call to the API
-        response = requests.get(url, params=parameters, headers=self.headers)
-
-        # check for a response
-        """
-            2xx  →  Success
-            3xx  →  Redirect (the resource moved somewhere else)
-            4xx  →  Your fault (bad request, not found, not authorised)
-            5xx  →  Their fault (server crashed, overloaded)
-        """
-        if response.status_code != 200:
-            raise ConnectionError(f"API returned {response.status_code}")
-        
-        # store the response in JSON format for readability
-        data = response.json()
-
-        # check the data for the "quotes" list (the list with the ticker). Return an empty list if missing
-        quotes = data.get("quotes", [])
+        results = yf.Search(company_name, max_results=5)
+        quotes = results.quotes
 
         if quotes:
             # the first list in quotes is the priority
